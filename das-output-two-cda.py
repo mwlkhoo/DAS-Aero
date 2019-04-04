270a5d083bb6d6d5c355ce08850aeb02584e4108
mwlkhoo <43212570+mwlkhoo@users.noreply.github.com>
2019-04-04 01:33:04 -0700

    Update das-output-two-cda.py

--- das-output-two-cda.py
+++ das-output-two-cda.py
@@ -1,4 +1,4 @@
-# TODO: Add file IO to write to file and reference relative to current filepath
+# IMPORTANT TODO: Check servo num, trigger, and boolean + test connection (mavlink -> TCP host)
 
 import re
 import sys
@@ -88,19 +88,23 @@ global habitatAlt
 
 
 # CDA = CDA1 = CDA2
-CDAAlt = None
+CDA1Alt = None
+CDA2Alt = None
 waterAlt = None
 habitatAlt = None
 
-defaultTrigger = 1600
+defaultTrigger = 1300
 
-CDATrigger = defaultTrigger
+CDA1Trigger = 1500
+CDA2Trigger = CDA1Trigger
 waterTrigger = defaultTrigger
 habitatTrigger = defaultTrigger
 
 # IMPORTANT TODO: Double check
 	 # servo number is correct
 	 # double check servo trigger and < vs. > is correct
+
+# RELEASE: Flip switch towards yourself (CDA u, water/habitat down)
 
 try: 
 	while(CDA1Alt == None or CDA2Alt == None or waterAlt == None or habitatAlt == None):
@@ -110,10 +114,11 @@ try:
 		# TODO: add while loop for error
 		altData = VFRData.alt
 
+		# Top left (ie. usual flight mod channel)
 		# open = smaller than 1500
 		if(CDA1Alt == None):
 			CDA1Servo = servoData.servo5_raw
-			if(CDA1Servo < CDATrigger):
+			if(CDA1Servo < CDA1Trigger):
 				CDA1AltMetres = altData
 				CDA1Alt = '%.3f'%(CDA1AltMetres * feetPerMetre)
 				# CDAOrder = order
@@ -128,10 +133,11 @@ try:
 				updateResults(body)
 				webbrowser.open(resultsUrl)
 
+		# Top left (ie. usual flight mod channel)
 		# open = smaller than 1500
 		if(CDA2Alt == None):
 			CDA2Servo = servoData.servo6_raw
-			if(CDA2Servo < CDATrigger):
+			if(CDA2Servo < CDA2Trigger):
 				CDA2AltMetres = altData
 				CDA2Alt = '%.3f'%(CDA2AltMetres * feetPerMetre)
 				# CDAOrder = order
@@ -147,7 +153,8 @@ try:
 				webbrowser.open(resultsUrl)
 
 
-
+		# Channel D (RCIN6) -> front hatch
+		# open = larger than 1300
 		if(waterAlt == None):
 			waterServo = servoData.servo7_raw
 			if(waterServo > waterTrigger):
@@ -163,7 +170,8 @@ try:
 				updateResults(body)
 				webbrowser.open(resultsUrl)
 
-
+		# Channel E (RCIN7) -> back hatch
+		# open = larger than 1300
 		if(habitatAlt == None):
 			habitatServo = servoData.servo8_raw
 			if(habitatServo > habitatTrigger):
